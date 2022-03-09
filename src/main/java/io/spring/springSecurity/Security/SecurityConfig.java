13665e859317fc9564f5be0152649775649663bd
Sohan Barman <barmansohan9@gmail.com>
2022-03-09 21:56:06 +0530

    create new user under User Details Service

--- src/main/java/io/spring/springSecurity/Security/SecurityConfig.java
+++ src/main/java/io/spring/springSecurity/Security/SecurityConfig.java
@@ -1,2 +1,43 @@
-package io.spring.springSecurity.Security;public class SecurityConfig {
+package io.spring.springSecurity.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorize->{ //this authorize object allow any request to the root
+            authorize.antMatchers("/","/webjar/**").permitAll(); // this webjar/** means all webjar url
+                })
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and()
+                .httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails user= User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+
+        UserDetails admin= User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("123")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
 }
--- src/test/java/io/spring/springSecurity/Controller/IndexControllerTestIT.java
+++ src/test/java/io/spring/springSecurity/Controller/IndexControllerTestIT.java
@@ -1,4 +1,51 @@
+package io.spring.springSecurity.Controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
 class IndexControllerTestIT {
-  
+
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc= MockMvcBuilders
+                .webAppContextSetup(wac)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    void testGetIndexSlash() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetUserWithHttpAuth() throws Exception {
+        mockMvc.perform(get("/user").with(httpBasic("user1","1234")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void TestUserDetailsNewUser() throws Exception {
+        mockMvc.perform(get("/admin").with(httpBasic("admin","123")))
+                .andExpect(status().isOk());
+    }
 }
